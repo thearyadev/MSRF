5d3cd70e74b6165034783a67e2d0755623f972c2
thearyadev <personal@aryankothari.dev>
2022-12-29 01:30:03 -0500

    invalid import fixed

--- util/models/dashboard_data.py
+++ util/models/dashboard_data.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from util import GeneratedDashboardDataModel
 from rich import print
 
+
 class DashboardData(GeneratedDashboardDataModel): ...
 
 
@@ -9,5 +10,6 @@ if __name__ == "__main__":
     p = "F:\Documents\Python Projects\MSRF-DEV\dashboard_data_schema_source.json"
     with open(p, "r") as f:
         import json
+
         dashboard = DashboardData(**json.load(f))
         print(dashboard.userStatus.levelInfo)
