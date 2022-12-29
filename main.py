20735f06f44331e767a9777735a0dbff4c8b8989
thearyadev <personal@aryankothari.dev>
2022-12-29 17:36:17 -0500

    add all components to gui & update requirements.txt

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import flet as ft
 
 def configure_loggers() -> list[None]:
     logging.basicConfig(
-        format='%(name)s----[%(threadName)s] [%(asctime)s] [%(levelname)s]'
+        format='[%(threadName)s] [%(levelname)s]'
                ' [%(filename)s] [Line %(lineno)d] %(message)s',
         handlers=[
             logging.FileHandler("farmer.log"),
@@ -104,30 +104,161 @@ def check_then_run():
     # run_sequential_threads(accounts=accounts_ready)
 
 
+def get_log() -> str:
+    with open("farmer.log", "r") as file:
+        return "".join(list((list(reversed(file.readlines()))[:100])))
+
+
+def remove_account(email):
+    logger.info(f"Removing {email}")
+
+
 def main_screen(page: ft.Page):
-    page.title = "Flet counter example"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.window_title_bar_hidden = True
+    page.window_title_bar_buttons_hidden = True
+    def show_bs(e):
+        bs.open = True
+        bs.update()
 
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+    def bs_dismissed(e):
+        logger.info("dismissed")
 
-    def minus_click(e):
-        txt_number.value = str(int(txt_number.value) - 1)
-        page.update()
+    def close_bs(e):
+        bs.open = False
+        bs.update()
 
-    def plus_click(e):
-        txt_number.value = str(int(txt_number.value) + 1)
-        page.update()
+    bs = ft.BottomSheet(
+        ft.Container(
+            ft.Column(
+                [
+                    ft.Text("Add Account", size=35),
+                    ft.Text("Note: Ensure that the Microsoft Rewards onboarding tasks have been completed. Make sure "
+                            "the credentials are correct.", italic=True, color=ft.colors.BLUE_GREY),
+                    ft.Row(
+                        [
+                            ft.TextField(label="Email"),
+                            ft.TextField(label="Password"),
+                            ft.ElevatedButton("Add", on_click=close_bs)
+                        ],
 
+                    )
+                ],
+                tight=True,
+            ),
+            padding=10,
+        ),
+        open=False,
+        on_dismiss=bs_dismissed,
+    )
+    page.overlay.append(bs)
+
+    page.title = "Microsoft Rewards Farmer"
+    page.window_height = 720
+    page.window_width = 1280
+    page.window_resizable = False
+
+    log_text = ft.Text(get_log(), font_family="Consolas", size=10, overflow=ft.TextOverflow.VISIBLE)
+    accountsTable = ft.DataTable(
+        width=600 if not page.web else 800,
+        columns=[
+            ft.DataColumn(ft.Text("Account")),
+            ft.DataColumn(ft.Text("Last Exec")),
+            ft.DataColumn(ft.Text("Points"), numeric=True),
+            ft.DataColumn(ft.Text("")),
+        ],
+        rows=[
+            ft.DataRow(
+                cells=[
+                    ft.DataCell(ft.Text(account.email)),
+                    ft.DataCell(
+                        ft.Text(
+                            f"{(((datetime.datetime.now(tz=datetime.timezone.utc) - account.lastExec).total_seconds() / 60) / 60):.0f} hrs ago")),
+                    ft.DataCell(ft.Text(str(account.points))),
+                    ft.DataCell(
+                        ft.IconButton(
+                            icon=ft.icons.CLOSE,
+                            tooltip="Remove Account",
+                            on_click=lambda e: [remove_account(e.control.data), hydrate()],
+                            data=account.email
+                        )
+                    )
+                ]
+
+            ) for account in db.read()
+        ],
+    )
     page.add(
         ft.Row(
             [
-                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
-                txt_number,
-                ft.IconButton(ft.icons.ADD, on_click=plus_click),
+                ft.WindowDragArea(
+                    ft.Container(ft.Text("Microsoft Rewards Farmer"),
+                                 bgcolor=ft.colors.TRANSPARENT, padding=10, margin=0), expand=True
+                ),
+                ft.IconButton(ft.icons.MINIMIZE, on_click=lambda _: page.window_close()),
             ],
-            alignment=ft.MainAxisAlignment.CENTER,
+            visible=not page.web,
+        ),
+        ft.Row(
+            [
+                ft.Column(
+                    controls=[
+                        ft.Container(
+                            alignment=ft.alignment.center,
+                            content=accountsTable,
+                            expand=True,
+                        ),
+                        ft.Row(
+                            [
+                                ft.IconButton(
+                                    icon=ft.icons.ADD,
+                                    tooltip="Add Account",
+                                    on_click=show_bs
+                                ),
+                            ]
+                        )
+                    ],
+
+                ),
+                ft.VerticalDivider(),
+                ft.Column(
+                    expand=True,
+                    controls=[log_text],
+                    scroll=ft.ScrollMode.ALWAYS,
+                ),
+            ],
+            spacing=0,
+            expand=True,
         )
     )
+
+    def hydrate():
+        accountsTable.rows = [
+            ft.DataRow(
+                cells=[
+                    ft.DataCell(ft.Text(account.email)),
+                    ft.DataCell(
+                        ft.Text(
+                            f"{(((datetime.datetime.now(tz=datetime.timezone.utc) - account.lastExec).total_seconds() / 60) / 60):.0f} hrs ago")),
+                    ft.DataCell(ft.Text(str(account.points))),
+                    ft.DataCell(
+                        ft.IconButton(
+                            icon=ft.icons.CLOSE,
+                            tooltip="Remove Account",
+                            on_click=lambda e: [remove_account(e.control.data), hydrate()],
+                            data=account.email
+                        )
+                    )
+                ]
+
+            ) for account in db.read()
+        ]
+
+        log_text.value = get_log()
+        page.update()
+
+    while True:
+        time.sleep(20)
+        hydrate()
 
 
 if __name__ == '__main__':
@@ -136,7 +267,15 @@ if __name__ == '__main__':
     # scheduler.start()
     # app.run(debug=not config.debug)
     # atexit.register(lambda: scheduler.shutdown())
-    ft.app(target=main_screen)
+
+    # for account in db.read():
+    #     util.exec_farmer(
+    #         account=account,
+    #         config=config,
+    #         db=db
+    #     )
+
+    ft.app(target=main_screen, view=ft.WEB_BROWSER)
 
 # PB PASSWORD C!ddKm9R5ESTJJz6
 #
