b7da9ae4f90a0ae8e229f5892e91db349b229fbd
thearyadev <personal@aryankothari.dev>
2022-12-30 16:17:13 -0500

    remove code used to test

--- util/browser/exec_single_account.py
+++ util/browser/exec_single_account.py
@@ -58,8 +58,6 @@ def exec_farmer(*, account: util.MicrosoftAccount, config: util.Config, db: data
     account.points = util.getPointCount(browser)  # will redirect to bing.com. Go back to baseurl
     db.write(account)
     logger.info(f"Current Points: {account.points}")
-    browser.quit()
-    return
 
     browser.get(BASE_URL)  # return
 
