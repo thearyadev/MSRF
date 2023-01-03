652f3f7269d3e391446acc50d61cc9d3bc448147
thearyadev <personal@aryankothari.dev>
2023-01-02 19:56:08 -0500

    Rewrite of old function using new Dashboard model.

--- util/browser/completeDailySet.py
+++ util/browser/completeDailySet.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 from datetime import datetime
 import urllib.parse
 from util import deprecated
+from rich import print
 
 
 @deprecated
@@ -68,17 +69,106 @@ def complete_daily_set(browser: WebDriver, base_url: str):
             util.resetTabs(browser, base_url)
 
 
+# noinspection PyTypeChecker
 def exec_daily_set(browser: WebDriver):
     """Completes the daily set section"""
     logger: logging.Logger = logging.getLogger("msrf")  # get logger
-    #accountData: util.DashboardData = util.load_dashboard_data(browser)
+    # accountData: util.DashboardData = util.load_dashboard_data(browser)
     # load the daily set data.
     with open("../../dashboard_data_schema_source.json", "r") as file:
         import json
         accountData = util.DashboardData(**json.load(file))
-        print(accountData.dailySetPromotions)
-    todayDate = datetime.today().strftime('%m/%d/%Y')
 
+    # the daily set is a few quizzes and such for the current date.
+    # the data will include a dictionary of "dates". yesterday, today, and tomorrow.
+    # only Today is can be done.
+    # from this dict, get the one from today.
+
+    # Get the current date. Parsed date has no time, replace all times with 0
+    # get data from dict. Will return None if there is no daily set.
+    daily_set: list[util.DailyPromotion] | None = accountData.dailySetPromotions.get(
+        datetime.now().replace(minute=0, hour=0, second=0, microsecond=0)
+    )
+
+    if daily_set is None:
+        logger.warning("Daily set did not yield any results. Dictionary containing the data does not contain the key "
+                       "for the current date.")
+
+    # if daily set was found
+    # iterate over all items of the daily set.
+
+    for daily_set_item in daily_set:
+        if not daily_set_item.complete:  # if the item hasn't been done yet
+            logger.info(f"Completing Daily Set Card #{daily_set_item.cardNumber}")
+
+            if daily_set_item.promotionType == "urlreward":
+                logger.info("Daily set item is of type [urlreward]")
+                try:
+                    logger.info(f"Completing daily set search for card #{daily_set_item.cardNumber}")
+                    util.complete_daily_set_search(browser, daily_set_item.cardNumber)
+                except Exception as e:
+                    logger.critical(f"Unable to complete daily_set_search due to an uncaught error in scraper. {e}")
+
+            if daily_set_item.promotionType == "quiz":
+                logger.info("Daily set item is of type [quiz]")
+                if daily_set_item.pointProgressMax == 50 and daily_set_item.pointProgress == 0:
+                    # if the point progress max is 50, it is a this_or_that quiz.
+                    # only complete if the pointProgress is == 0 (untouched)
+                    logger.info(f"Completing this or that for card #{daily_set_item.cardNumber}")
+                    try:
+                        util.complete_daily_set_this_or_that(browser, daily_set_item.cardNumber,
+                                                             base_url="https://rewards.bing.com")
+                    except Exception as e:
+                        logger.critical(
+                            f"Unable to complete daily_set_this_or_that due to an uncaught error in scraper. {e}")
+                elif daily_set_item.pointProgressMax in (40, 30) and daily_set_item.pointProgressMax == 0:
+                    # if the max points are 40 or 30, it is just a generic quiz
+                    # only attempt if progress on the quiz is 0
+                    logger.info(f"Completing quiz of card #{daily_set_item.cardNumber}")
+                    try:
+                        util.complete_daily_set_quiz(browser,
+                                                     daily_set_item.cardNumber,
+                                                     base_url="https://rewards.bing.com")
+                    except Exception as e:
+                        logger.critical(
+                            f"Unable to complete daily_set_quiz due to an uncaught error in scraper. {e}")
+
+                elif daily_set_item.pointProgressMax == 10 and daily_set_item.pointProgress == 0:
+                    # if the total point value is 10 and the point progress is 0 (untouched)
+                    # this block is legacy code. Not sure what it does exactly, but it remains here in case it breaks
+                    try:
+                        searchUrl = urllib.parse.unquote(
+                            urllib.parse.parse_qs(
+                                # url query params dict
+                                # get ru, then first element of a list
+                                urllib.parse.urlparse(daily_set_item.destinationUrl).query)['ru'][0]
+
+                        )
+                        # something?
+                        searchUrlQueries = urllib.parse.parse_qs(urllib.parse.urlparse(searchUrl).query)
+                        filters = {}
+                        for fltr in searchUrlQueries['filters'][0].split(" "):
+                            fltr = fltr.split(':', 1)
+                            filters[fltr[0]] = fltr[1]
+                        if "PollScenarioId" in filters:
+                            logger.info(f"Completing poll of card #{daily_set_item.cardNumber}")
+                            try:
+                                util.completeDailySetSurvey(browser, daily_set_item.cardNumber)
+                            except Exception as e:
+                                logger.critical(
+                                    f"Unable to complete daily_set_survey due to an uncaught error in scraper. {e}")
+                        else:
+                            logger.info(f"Completing quiz of card #{daily_set_item.cardNumber}")
+                            try:
+                                util.completeDailySetVariableActivity(browser, daily_set_item.cardNumber)
+                            except Exception as e:
+                                logger.critical(
+                                    f"Unable to complete daily_set_variable_activity due to an uncaught error in "
+                                    f"scraper. {e}")
+                    except Exception as e:
+                        logger.critical(f"Legacy code segment caused an exception. {e}")
+        else:
+            logger.info(f"Daily Set Card #{daily_set_item.cardNumber} is already complete")
 
 if __name__ == '__main__':
     exec_daily_set(0)
