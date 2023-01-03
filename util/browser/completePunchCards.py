1a80c1a0c710a3731620c4fcb3d24033d7157ee9
thearyadev <personal@aryankothari.dev>
2023-01-03 17:37:49 -0500

    Deprecate old module and create rewrite to support new dashboard data format

--- util/browser/completePunchCards.py
+++ util/browser/completePunchCards.py
@@ -8,7 +8,7 @@ import time
 from util import deprecated
 
 
-
+@deprecated
 def complete_punch_cards(browser: WebDriver, base_url: str):
     logger: logging.Logger = logging.getLogger("msrf")  # get logger
     punchCards = util.getDashboardData(browser)['punchCards']
@@ -21,7 +21,7 @@ def complete_punch_cards(browser: WebDriver, base_url: str):
                 if base_url == "https://rewards.microsoft.com":
                     logger.info("Completing single punch card")
                     util.complete_punch_card(browser, punchCard['parentPromotion']['attributes']['destination'],
-                                           punchCard['childPromotions'])
+                                             punchCard['childPromotions'])
                 else:
                     url = punchCard['parentPromotion']['attributes']['destination']
                     path = url.replace(
@@ -37,3 +37,35 @@ def complete_punch_cards(browser: WebDriver, base_url: str):
     logger.info("Returning home.")
     browser.get(base_url)
     time.sleep(2)
+
+
+def exec_punch_cards(browser: WebDriver):
+    logger: logging.Logger = logging.getLogger("msrf")  # get logger
+
+    accountData: util.DashboardData = util.load_dashboard_data(browser)
+    if accountData is None:
+        logging.critical("Unable to complete punch cards due to missing dashboard data.")
+        return
+
+    punch_cards: list[util.PunchCards] = accountData.punchCards
+
+    if not punch_cards:
+        logging.critical("Unable to complete punch cards. Attribute is None or Empty Array")
+        return
+
+    for punch_card in punch_cards:
+        if punch_card.parentPromotion is not None \
+                and punch_card.childPromotion is not None \
+                and not punch_card.parentPromotion.complete \
+                and punch_card.parentPromotion.pointProgressMax != 0:
+            logger.info(f"Valid punch card child promotion found: {punch_card.name}")
+            try:
+                util.exec_single_punch_card_child_promotion(
+                    browser,
+                    punch_card.parentPromotion.destinationUrl,
+                    punch_card.childPromotion
+                )
+            except Exception as e:
+                logging.critical(f"Failed to complete punch card child promotion. Expected error: {e}")
+            else:
+                logger.info(f"Successfully completed punch card child promotion: {punch_card.name}")
