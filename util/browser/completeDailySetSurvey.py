364d130969acd0db404571c43bda43cceff83486
thearyadev <personal@aryankothari.dev>
2023-01-03 00:13:19 -0500

    formatting

--- util/browser/completeDailySetSurvey.py
+++ util/browser/completeDailySetSurvey.py
@@ -3,15 +3,17 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 import random
 
+
 def completeDailySetSurvey(browser: WebDriver, cardNumber: int):
     time.sleep(5)
-    browser.find_element(By.XPATH, '//*[@id="daily-sets"]/mee-card-group[1]/div/mee-card[' + str(cardNumber) + ']/div/card-content/mee-rewards-daily-set-item-content/div/a').click()
+    browser.find_element(By.XPATH, '//*[@id="daily-sets"]/mee-card-group[1]/div/mee-card[' + str(
+        cardNumber) + ']/div/card-content/mee-rewards-daily-set-item-content/div/a').click()
     time.sleep(1)
-    browser.switch_to.window(window_name = browser.window_handles[1])
+    browser.switch_to.window(window_name=browser.window_handles[1])
     time.sleep(8)
     browser.find_element(By.ID, "btoption" + str(random.randint(0, 1))).click()
     time.sleep(random.randint(10, 15))
     browser.close()
     time.sleep(2)
-    browser.switch_to.window(window_name = browser.window_handles[0])
+    browser.switch_to.window(window_name=browser.window_handles[0])
     time.sleep(2)
