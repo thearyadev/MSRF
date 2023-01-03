12c434e6bca39ff643698b5b1d8fe7de9fbff7de
thearyadev <personal@aryankothari.dev>
2023-01-02 19:55:40 -0500

    Update name of data model

--- util/models/dashboard_data.py
+++ util/models/dashboard_data.py
@@ -1,8 +1,8 @@
 import typing
 
 from pydantic import BaseModel, root_validator
-from .dashboard_json_models import GeneratedDashboardDataModel
+from .dashboard_json_models import DashboardDataModel
 from rich import print
 
 
-class DashboardData(GeneratedDashboardDataModel): ...
+class DashboardData(DashboardDataModel): ...
