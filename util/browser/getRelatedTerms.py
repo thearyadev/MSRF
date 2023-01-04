15c95a740be13b5d5ea5adb8cf0be1b11d0e2547
thearyadev <personal@aryankothari.dev>
2023-01-03 23:41:12 -0500

    Add logging

--- util/browser/getRelatedTerms.py
+++ util/browser/getRelatedTerms.py
@@ -1,9 +1,14 @@
+import logging
+
 import requests
 
 
-def getRelatedTerms(word: str, pc_user_agent: str) -> list:
+def getRelatedTerms(word: str, pc_user_agent: str) -> list[str]:
+    logger: logging.Logger = logging.getLogger("msrf")
+
     try:
         r = requests.get('https://api.bing.com/osjson.aspx?query=' + word, headers={'User-agent': pc_user_agent})
         return r.json()[1]
-    except:
+    except Exception as e:
+        logger.critical(f"Unable to get related search terms. {e}")
         return []
