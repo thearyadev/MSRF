9fa6b5e277728b8b336493a87f1e70d4ef4a8c2e
thearyadev <personal@aryankothari.dev>
2023-01-05 01:42:37 -0500

    script to delete and create new build

---
+++ scripts/build.bat
@@ -0,0 +1,8 @@
+ECHO OFF
+ECHO deleting dist directory
+del dist
+flet pack main.py --name msrf --product-name "Microsoft Rewards Farmer" --product-version "0.9" --file-description "Microsoft Rewards Farmer" --icon "assets/msrf.ico"
+mkdir dist/bin
+xcopy bin "dist/bin" /e
+xcopy configuration.yaml dist
+ECHO build complete
