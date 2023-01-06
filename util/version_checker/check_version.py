c7ba86b47d61eb6c5ccfed5ff0b3274de722118a
thearyadev <personal@aryankothari.dev>
2023-01-05 19:31:38 -0500

    implement check_version.py

---
+++ util/version_checker/check_version.py
@@ -0,0 +1,16 @@
+from github import Github
+import github
+from pydantic import BaseModel
+
+
+class VersionInfo(BaseModel):
+    release_url: str
+    release_version: str
+
+
+def check_version() -> VersionInfo:
+    try:
+        d = Github().get_repo("thearyadev/MSRF").get_latest_release()
+        return VersionInfo(release_url=d.html_url, release_version=d.tag_name)
+    except Exception as e:
+        return VersionInfo(release_url="", release_version="")
