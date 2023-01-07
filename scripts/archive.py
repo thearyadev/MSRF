72db406371714f0a4f76f7ee0982134c1c59a184
thearyadev <personal@aryankothari.dev>
2023-01-07 14:54:19 -0500

    Change zipped name to include version number

--- scripts/archive.py
+++ scripts/archive.py
@@ -1,5 +1,10 @@
 import logging
 import shutil
+from types import SimpleNamespace
+
+import yaml
 
 if __name__ == '__main__':
-    shutil.make_archive("./dist/msrf-windows-64", "zip", "./dist")
+    with open("configuration.yaml", "r") as file:
+        config = SimpleNamespace(**yaml.safe_load(file))
+    shutil.make_archive(f"./deployable/msrf-windows-64_{config.version}", "zip", "./dist")
