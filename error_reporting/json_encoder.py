21632f298c47b3486183940d1f38075c975331d7
thearyadev <personal@aryankothari.dev>
2023-01-07 20:53:45 -0500

    Add JSON encoder to allow for dict keys to be encoded to json serializable (used for datetime.datetime keys)

---
+++ error_reporting/json_encoder.py
@@ -0,0 +1,23 @@
+import json
+import datetime
+
+
+class DateTimeEncoder(json.JSONEncoder):
+    """I stole this code from stackoverflow. It's an encoder that supports encoding Dict Keys for the json module."""
+
+    def _preprocess_date(self, obj):
+        if isinstance(obj, (datetime.date, datetime.datetime, datetime.timedelta)):
+            return obj.isoformat()
+        elif isinstance(obj, dict):
+            return {self._preprocess_date(k): self._preprocess_date(v) for k, v in obj.items()}
+        elif isinstance(obj, list):
+            return [self._preprocess_date(i) for i in obj]
+        return obj
+
+    def default(self, obj):
+        if isinstance(obj, (datetime.date, datetime.datetime, datetime.timedelta)):
+            return obj.isoformat()
+        return super().default(obj)
+
+    def iterencode(self, obj, **kwargs):
+        return super().iterencode(self._preprocess_date(obj))
