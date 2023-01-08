8edf2718a44ec52332f1408daf5466aa01acb662
thearyadev <personal@aryankothari.dev>
2023-01-07 20:58:29 -0500

    minor changes to script, add error_reporting module

--- scripts/sort_imports.py
+++ scripts/sort_imports.py
@@ -6,19 +6,20 @@ import subprocess
 
 def exec_file(file: str):
     if "__init__" not in file:  # do not run in __init__.py files
+        print(f"running on file: {file}")
         subprocess.run(
             f"autoflake --in-place --remove-unused-variables --remove-all-unused-imports  {file}")
         isort.file(file)
 
 
 if __name__ == '__main__':
-    dirs = ["custom_logging", "database", "util", "tests"]
+    dirs = ["custom_logging", "database", "util", "tests", "error_reporting"]
     files = ["main.py", ]
 
-    for d in track(dirs, "stage 1"):
+    for d in track(dirs, "stage: dirs"):
         gb = glob.glob(f"{d}/**/*.py", recursive=True)
         for f in gb:
             exec_file(f)
 
-    for f in track(files, "stage 2"):
+    for f in track(files, "stage 2: files"):
         exec_file(f)
