be021f3fa9668c2b0b6a1a0083d237631573cb11
thearyadev <personal@aryankothari.dev>
2023-01-23 13:52:31 -0500

    rebuild browser init function. Add support for headless Docker installation.

--- util/browser/browser_setup.py
+++ util/browser/browser_setup.py
@@ -11,7 +11,7 @@ import custom_logging
 import util
 
 
-def init_browser(*, headless: bool, agent: str) -> WebDriver:
+def init_browser_legacy(*, headless: bool, agent: str) -> WebDriver:
     """
     Initializes a new Chrome instance using Selenium and chromedriver.
 
@@ -50,3 +50,44 @@ def init_browser(*, headless: bool, agent: str) -> WebDriver:
             driver = webdriver.Chrome(options=options)
         atexit.register(driver.quit)
         return driver
+
+
+def init_browser(*, headless: bool, agent: str, execution_mode: str) -> WebDriver:
+    logger: custom_logging.FileStreamLogger = custom_logging.FileStreamLogger(console=True, colors=True)
+    logger.info(f"Starting browser in {execution_mode}")
+    driver: WebDriver | None = None
+
+    options = Options()
+    options.add_argument("user-agent=" + agent)
+    options.add_argument("log-level=3")
+    if headless:
+        options.add_argument("--headless")
+
+    if execution_mode == "DOCKER":  # This is linux installation
+        options.add_argument("--no-sandbox")  # causes chrome to crash in screen-less environment
+        options.add_argument("--disable-dev-shm-usage")  # causes chrome to crash in screen-less environment
+        driver = webdriver.Chrome(options=options)
+
+    if execution_mode == "DEV_LINUX":  # This is linux installation
+        ...
+
+    if execution_mode == "DEV_WINDOWS":  # This is Windows Installation
+        driver = webdriver.Chrome(options=options)
+
+    if execution_mode == "DIST_WINDOWS":  # This is Windows installation, but for distribution.
+        options.binary_location = "bin/chrome/chrome.exe"
+        options.add_argument("--log-level=OFF")
+        args = ["hide_console", ]
+        service = Service("bin/chromedriver.exe", service_args=args)
+        try:
+            service.creation_flags = subprocess.CREATE_NO_WINDOW
+        except AttributeError:
+            pass
+        driver = webdriver.Chrome(service=service, options=options)
+
+    if driver is None:
+        logger.critical("Unable to initialize browser. Error unknown.")
+        raise Exception("Unknown error")
+
+    atexit.register(driver.quit)
+    return driver
