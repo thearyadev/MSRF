016a8c7f02daffc609f41b093ebb13e5b712b41f
thearyadev <personal@aryankothari.dev>
2023-01-24 14:01:59 -0500

    complete most punch cards

--- util/browser/completePunchCard.py
+++ util/browser/completePunchCard.py
@@ -11,15 +11,20 @@ import util
 def exec_single_punch_card_child_promotion(browser: WebDriver,
                                            url: str,
                                            childPromotions: list['util.PunchCardChildPromotion']):
+    from util import ErrorReport, ErrorReporter
+
     logger: custom_logging.FileStreamLogger = custom_logging.FileStreamLogger(console=True, colors=True)
     logger.info(f"Navigating to punch card url: {url}")
     browser.get(url)
-
-    for child in childPromotions:
-        logger.info("Completing single childPromotion")
-        if not child.complete:
-            if child.promotionType == "urlreward":
-                browser.execute_script("document.getElementsByClassName('offer-cta')[0].click()")
+    time.sleep(2)
+    childPromotions.pop(0)
+    for childPromotion in childPromotions:
+        logger.info("Completing single punch card childPromotion")
+        if not childPromotion.complete:
+            if childPromotion.promotionType == "urlreward":
+                browser.find_elements(By.CLASS_NAME, "offer-cta")[1] \
+                    .find_element(By.TAG_NAME, "button") \
+                    .click()
                 time.sleep(1)
                 browser.switch_to.window(window_name=browser.window_handles[1])
                 time.sleep(random.randint(13, 17))
@@ -27,15 +32,20 @@ def exec_single_punch_card_child_promotion(browser: WebDriver,
                 time.sleep(2)
                 browser.switch_to.window(window_name=browser.window_handles[0])
                 time.sleep(2)
-            if child.promotionType == "quiz":
-                browser.execute_script("document.getElementsByClassName('offer-cta')[0].click()")
+
+            if childPromotion.promotionType == "quiz":
+                continue
+                browser.find_elements(By.CLASS_NAME, "offer-cta")[0] \
+                    .find_element(By.TAG_NAME, "button") \
+                    .click()  # click punch card button
+
                 time.sleep(1)
-                browser.switch_to.window(window_name=browser.window_handles[1])
-                time.sleep(8)
-                counter = str(
-                    browser.find_element(By.XPATH, '//*[@id="QuestionPane0"]/div[2]').get_attribute('innerHTML'))[:-1][
-                          1:]
-                numberOfQuestions = max([int(s) for s in counter.split() if s.isdigit()])
+                browser.switch_to.window(window_name=browser.window_handles[1])  # move browser to newly opened tab
+                time.sleep(8)  # waiting for quiz to load
+                pointsMax = int(browser.find_element(By.CLASS_NAME, "rqMCredits").text)
+
+                numberOfQuestions = int(pointsMax / 10)
+
                 for question in range(numberOfQuestions):
                     browser.execute_script(
                         'document.evaluate("//*[@id=\'QuestionPane' + str(question) + '\']/div[1]/div[2]/a[' + str(
@@ -45,6 +55,9 @@ def exec_single_punch_card_child_promotion(browser: WebDriver,
                     browser.find_element(By.XPATH, '//*[@id="AnswerPane' + str(
                         question) + '"]/div[1]/div[2]/div[4]/a/div/span/input').click()
                     time.sleep(3)
+
+
+
                 time.sleep(5)
                 browser.close()
                 time.sleep(2)
